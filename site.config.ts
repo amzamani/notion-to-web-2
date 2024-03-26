960e7f7e3b1d6fb23db243ce46c14da6940ced43
amzamani <62957837+amzamani@users.noreply.github.com>
2024-03-26 17:27:07 +0000

    social changes
    Signed-off-by: amzamani <62957837+amzamani@users.noreply.github.com>

--- site.config.ts
+++ site.config.ts
@@ -14,19 +14,19 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'ToffeeMoney Blog',
-  domain: 'blog.toffeemoney.com/',
+  name: 'Znap Product Studio',
+  domain: 'znap.in/',
   author: 'Zamani Tech Ventures Pvt Ltd',
 
   // open graph metadata (optional)
   description:
-    'ToffeeMoney Blog: Say hello to organised spending and savvy shopping',
+    'Znap Product Studio',
 
   // social usernames (optional)
-  // twitter: '',
+  twitter: 'znap_in',
   // github: '',
-  linkedin: '/company/toffeemoney/',
-  instagram: '/toffeemoney/',
+  linkedin: '/company/znap-products/',
+  instagram: 'znap.in',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '@toffeemoney', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -57,16 +57,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '24dd473a39584d70828468148b3b5bc3'
+    },
+    {
+      title: 'Contact',
+      pageId: 'c4f5dab6339e415e91d96c3b32bfb0d1'
+    }
+  ]
 })
